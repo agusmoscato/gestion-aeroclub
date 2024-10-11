c455d4b3da6f5997096f15d3c6fb4af1a8af04c1
Agustin Moscato <agustinmoscato369@gmail.com>
2024-10-11 14:46:02 -0300

    Inicializo el front de la aplicacion

---
+++ src/views/Asociado/AsociadoDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AsociadoDashboard() {
+  return (
+    <div className="content">
+      <h1>Dashboard de Asociado</h1>
+      <p>Aquí puedes ver el resumen de tu cuenta, horas de vuelo, y más.</p>
+    </div>
+  );
+}
+
+export default AsociadoDashboard;
